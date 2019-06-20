7eecd78578b1eac2881dc8760de0da6ccde228e9
InvisibleForest <grig.d.a@gmail.com>
2019-06-21 00:00:24 +0300

    update bucket button test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 def test_guest_should_see_basket_link(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
-    browser.find_element_by_class_name('btn-add-to-basket')
+    assert browser.find_element_by_class_name('btn-add-to-basket'), "button doesn't exist"
 
     
     
